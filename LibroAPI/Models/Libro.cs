a7a8a09fa167e02c56cbe14f798bb85cf21029e0
karivas-dev <72422927+karivas-dev@users.noreply.github.com>
2024-08-29 08:50:44 -0600

    Initial commit

---
+++ LibroAPI/Models/Libro.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibroAPI.Models
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Titulo { get; set; }
+        public string? Autor { get; set; }
+        public string? AnioPublicacion { get; set; }
+    }
+}
---
+++ ProductoAPI/Models/ProductoContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductoAPI.Models
+{
+    public class ProductoContext : DbContext
+    {
+        public ProductoContext(DbContextOptions<ProductoContext> options) : base(options) 
+        {
+        
+        }
+
+        public DbSet<Producto> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Producto>().HasData(
+                new Producto
+                {
+                    Id = 1,
+                    Nombre = "Laptop",
+                    Categoria = "Electrónica",
+                    Descripcion = "Una laptop de alto rendimiento."
+                },
+                new Producto
+                {
+                    Id = 2,
+                    Nombre = "Smartphone",
+                    Categoria = "Electrónica",
+                    Descripcion = "Una smartphone de nueva generación."
+                },
+                new Producto
+                {
+                    Id = 3,
+                    Nombre = "Silla de escritorio",
+                    Categoria = "Muebles",
+                    Descripcion = "Una silla de escritorio cómoda."
+                }
+            );
+        }
+    }
+}
